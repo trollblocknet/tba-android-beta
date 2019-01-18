ff2a61e911f31f7d41b9fb09f9294603638c2e3f
TBN <trollblockdb2018a@gmail.com>
2019-01-18 05:44:33 +0100

    no message

--- app/src/main/java/cat/trollblocknet/tba_android_beta_13/FaqFragment.java
+++ app/src/main/java/cat/trollblocknet/tba_android_beta_13/FaqFragment.java
@@ -13,5 +13,16 @@ public class FaqFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_faq, container, false);
+
+        // SHOW EMBEDED HTML
+            /*WebView wv = (WebView) findViewById(R.id.tw_WebView);
+
+            final String mimeType = "text/html";
+            final String encoding = "UTF-8";
+            String html = "<blockquote class=\"twitter-tweet\" data-lang=\"en\"><p lang=\"es\" dir=\"ltr\">El clan Puyol 3.000.000.000€<br>Te lo pongo aquí por si lees que España nos roba.</p>&mdash; Te la han Colau (@DelIndepe) <a href=\"https://twitter.com/DelIndepe/status/1085855149497638913?ref_src=twsrc%5Etfw\">January 17, 2019</a></blockquote>\n" +
+                    "<script async src=\"https://platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>\n";
+
+
+            wv.loadDataWithBaseURL("", html, mimeType, encoding, "");*/
     }
 }
--- app/src/main/java/cat/trollblocknet/tba_android_beta_13/HandleShareAction.java
+++ app/src/main/java/cat/trollblocknet/tba_android_beta_13/HandleShareAction.java
@@ -1,15 +1,40 @@
 package cat.trollblocknet.tba_android_beta_13;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.util.StringBuilderPrinter;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.tweetui.BaseTweetView;
+import com.twitter.sdk.android.tweetui.CompactTweetView;
+import com.twitter.sdk.android.tweetui.TweetLinkClickListener;
+import com.twitter.sdk.android.tweetui.TweetUi;
+import com.twitter.sdk.android.tweetui.TweetUtils;
+import com.twitter.sdk.android.tweetui.TweetView;
+import com.twitter.sdk.android.tweetui.internal.TweetMediaUtils;
+import com.twitter.sdk.android.tweetui.internal.TweetMediaView;
 
 import org.apache.commons.io.FilenameUtils;
 
@@ -21,15 +46,17 @@ import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.LinkedBlockingDeque;
 
-/*import com.twitter.sdk.android.core.Callback;
-import com.twitter.sdk.android.core.models.Tweet;
-import com.twitter.sdk.android.core.TwitterException;
-import com.twitter.sdk.android.tweetui.TweetUtils;
-import com.twitter.sdk.android.tweetui.TweetView;*/
+import static java.lang.System.exit;
+
 
 public class HandleShareAction extends AppCompatActivity {
 
     private static final String CLOUDAMQP_URL = "amqp://mbsfxvbl:w_W5BK8P4iy_GQucoyYA63AlSzPOEjWM@raven.rmq.cloudamqp.com/mbsfxvbl";
+    private static final String tw_consumerKey = "UnFTQTeTx2tm98zQwG1jLhL3g";
+    private static final String tw_consumerSecret = "HAR8c3Rpjo7ZEQzgAHPLY4lGb7XtjSwa1gfLd3SirjJOX12GUa";
+
+    private String stringURL;
+    private String TweetId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,67 +72,78 @@ public class HandleShareAction extends AppCompatActivity {
         String type = intent.getType();
 
         if (Intent.ACTION_SEND.equals(action) && type != null) {
-            if ("text/plain".equals(type)) {
-                String stringURL = handleSendText(intent); // Handle text being sent
 
-                // WE OBTAIN TWEET ID USING THE FILEUTILS LIBRARY TO GET THE FILENAME AND SPLITTING THE PARAMETERS AFTER THE FIRST "?" MARK ->
-                //
-                // https://commons.apache.org/proper/commons-io/javadocs/api-2.4/org/apache/commons/io/FilenameUtils.html
-                // https://stackoverflow.com/questions/14959763/java-remove-all-characters-after-point/14959970
-
-                String TweetID = FilenameUtils.getBaseName(stringURL).split("\\?", 2)[0];
-
-                // PRINTF - REMOVE
-                setContentView(R.layout.activity_handle_share_action);
-                TextView textView = (TextView) findViewById(R.id.SharedURL);
-                textView.setText(TweetID);
-                //END PRINTF
-
-                // LOAD TWEET LAYOUT INTO THE CORRESPONDING ACTIVITY LAYOUT FRAME. WE USE THE TWEETUTILS LIBRARY TO DO SO -->
-                //
-                // https://github.com/twitter/twitter-kit-android/wiki/Show-Tweets
-
-                /*final LinearLayout myLayout
-                        = (LinearLayout) findViewById(R.id.my_tweet_layout);
-
-                final long tweetId = 510908133917487104L;
-                TweetUtils.loadTweet(tweetId, new Callback<Tweet>() {
-                    @Override
-                    public void success(Result<Tweet> result) {
-                        myLayout.addView(new TweetView(EmbeddedTweetsActivity.this, tweet));
-                    }
-
-                    @Override
-                    public void failure(TwitterException exception) {
-                        // Toast.makeText(...).show();
-                    }
-                });*/
-
-                //CARREGAR LA RESTA DEL FORMULARI
-
-                //PAS FINAL: ENVIAR MISSATGE A CLOUDAMQP (BOTÓ SEND A LA ACTION BAR)
-
-                SendAMQPMessage(stringURL);
-            }
+                stringURL = handleSendText(intent); // Handle text being sent
+                TweetId = FilenameUtils.getBaseName(stringURL).split("\\?", 2)[0];
 
         }  else {
-            // TOAST / DIALOG - L'OBJECTE COMPARTIT NO CORRESPON A UN TWEET
+            // TOAST / DIALOG - L'OBJECTE COMPARTIT NO CORRESPON A UN STRING
+            finish();
+            exit(0);
         }
 
-    }
+        // RENDER TWEET
 
-    // SEND MESSAGE (URL) TO THE RABBITMQ AMQP QUEUE
+        final RelativeLayout myLayout
+                = (RelativeLayout) findViewById(R.id.tweet_layout);
+
+        TwitterAuthConfig authConfig
+                = new TwitterAuthConfig(tw_consumerKey, tw_consumerSecret);
+
+        final TwitterConfig config = new TwitterConfig.Builder(this)
+                .twitterAuthConfig(authConfig)
+                .debug(true)
+                .build();
+        Twitter.initialize(config);
+
+        // INITIALIZE CONFIG
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                TweetUi.getInstance();
+                Log.i("ReactTwitterKit", "TweetUi instance initialized");
+            }
+        }).start();
+
+        // INFLATE TWEET
+
+        TweetUtils.loadTweet(Long.valueOf(TweetId), new Callback<Tweet>() {
+
+            @Override
+            public void success(Result<Tweet> result) {
+
+                //myLayout.setClickable(false);
+                myLayout.addView(new CompactTweetView(HandleShareAction.this, result.data));
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                // Toast.makeText(...).show();
+            }
+        });
+
+    }
 
     String handleSendText(Intent intent) {
         String sharedURL = intent.getStringExtra(Intent.EXTRA_TEXT);
         if (sharedURL != null) {
             // Update UI to reflect text being shared
-            setContentView(R.layout.activity_handle_share_action);
+
+            // ONLY TWEET URL
+            /*setContentView(R.layout.activity_handle_share_action);
             TextView textView = (TextView) findViewById(R.id.SharedURL);
-            textView.setText(sharedURL);
+            textView.setText(sharedURL);*/
+
+            // API TWEET
+
+            // AUTHORIZE API
+
            }
         return sharedURL;
     }
+
+    // SEND MESSAGE (URL) TO THE RABBITMQ AMQP QUEUE
 
     Thread publishThread;
     @Override
@@ -127,6 +165,25 @@ public class HandleShareAction extends AppCompatActivity {
     }
 
     ConnectionFactory factory = new ConnectionFactory();
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.handle_share_action_bar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // handle button activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.TopBarSendButton) {
+            //send cluodamqp message and return to parent activity / external app
+            SendAMQPMessage(stringURL);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private void setupConnectionFactory() {
         String uri = CLOUDAMQP_URL;
         try {
@@ -137,8 +194,7 @@ public class HandleShareAction extends AppCompatActivity {
         }
     }
 
-    public void publishToAMQP()
-    {
+    public void publishToAMQP() {
         publishThread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -182,4 +238,27 @@ public class HandleShareAction extends AppCompatActivity {
         publishMessage(sharedURL); // Enqueue message in local queue (it will be automatically re-routed to the rabbitMQ queue
 
     }
+
+    public void initTwitterKit(Context reactContext) {
+        if (tw_consumerKey == null || tw_consumerSecret == null) {
+            return;
+        }
+
+        TwitterAuthConfig authConfig
+                = new TwitterAuthConfig(tw_consumerKey, tw_consumerSecret);
+
+        final TwitterConfig config = new TwitterConfig.Builder(reactContext)
+                .twitterAuthConfig(authConfig)
+                .debug(true)
+                .build();
+        Twitter.initialize(config);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                TweetUi.getInstance();
+                Log.i("ReactTwitterKit", "TweetUi instance initialized");
+            }
+        }).start();
+    }
+    
 }
