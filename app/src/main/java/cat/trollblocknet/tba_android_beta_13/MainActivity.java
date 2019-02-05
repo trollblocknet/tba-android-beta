078cd5a04da86a67f37ad274ef8a982f28d718f5
TBN <trollblockdb2018a@gmail.com>
2019-02-06 00:42:07 +0100

    no message

--- app/src/main/java/cat/trollblocknet/tba_android_beta_13/MainActivity.java
+++ app/src/main/java/cat/trollblocknet/tba_android_beta_13/MainActivity.java
@@ -1,5 +1,6 @@
 package cat.trollblocknet.tba_android_beta_13;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -77,6 +78,9 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent myIntent = new Intent(MainActivity.this, SettingsActivity.class);
+            //myIntent.putExtra("key", value); //Optional parameters
+            MainActivity.this.startActivity(myIntent);
             return true;
         }
         else if (id == R.id.action_logout) {
